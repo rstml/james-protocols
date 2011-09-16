2360949354ee5cbdaca76b56d1d827cbdeb970e6
Norman Maurer <norman@apache.org>
2011-09-16 10:57:30 +0000

    Add simple SMTPServer by using netty. See PROTOCOLS-31
    git-svn-id: https://svn.apache.org/repos/asf/james/protocols/trunk@1171501 13f79535-47bb-0310-9956-ffa450edef68

---
+++ smtp-netty/src/main/java/org/apache/james/protocols/smtp/netty/SMTPNettySession.java
@@ -0,0 +1,207 @@
+/****************************************************************
+ * Licensed to the Apache Software Foundation (ASF) under one   *
+ * or more contributor license agreements.  See the NOTICE file *
+ * distributed with this work for additional information        *
+ * regarding copyright ownership.  The ASF licenses this file   *
+ * to you under the Apache License, Version 2.0 (the            *
+ * "License"); you may not use this file except in compliance   *
+ * with the License.  You may obtain a copy of the License at   *
+ *                                                              *
+ *   http://www.apache.org/licenses/LICENSE-2.0                 *
+ *                                                              *
+ * Unless required by applicable law or agreed to in writing,   *
+ * software distributed under the License is distributed on an  *
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY       *
+ * KIND, either express or implied.  See the License for the    *
+ * specific language governing permissions and limitations      *
+ * under the License.                                           *
+ ****************************************************************/
+package org.apache.james.protocols.smtp.netty;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.net.ssl.SSLEngine;
+
+import org.apache.james.protocols.api.LineHandler;
+import org.apache.james.protocols.impl.AbstractSession;
+import org.apache.james.protocols.impl.LineHandlerUpstreamHandler;
+import org.apache.james.protocols.smtp.SMTPConfiguration;
+import org.apache.james.protocols.smtp.SMTPSession;
+import org.jboss.netty.channel.Channel;
+import org.slf4j.Logger;
+
+/**
+ * {@link SMTPSession} implementation for use with Netty
+ */
+public class SMTPNettySession extends AbstractSession implements SMTPSession {
+    public final static String SMTP_SESSION = "SMTP_SESSION";
+
+    private boolean relayingAllowed;
+
+    private Map<String, Object> connectionState;
+
+    private SMTPConfiguration theConfigData;
+
+    private int lineHandlerCount = 0;
+
+    public SMTPNettySession(SMTPConfiguration theConfigData, Logger logger, Channel channel, SSLEngine engine) {
+        super(logger, channel, engine);
+        this.theConfigData = theConfigData;
+        connectionState = new HashMap<String, Object>();
+
+        relayingAllowed = theConfigData.isRelayingAllowed(getRemoteIPAddress());
+    }
+
+    public SMTPNettySession(SMTPConfiguration theConfigData, Logger logger, Channel channel) {
+        this(theConfigData, logger, channel, null);
+    }
+
+    /**
+     * @see org.apache.james.protocols.smtp.SMTPSession#getConnectionState()
+     */
+    public Map<String, Object> getConnectionState() {
+        return connectionState;
+    }
+
+    /**
+     * @see org.apache.james.protocols.smtp.SMTPSession#getState()
+     */
+    @SuppressWarnings("unchecked")
+    public Map<String, Object> getState() {
+        Map<String, Object> res = (Map<String, Object>) getConnectionState().get(SMTPSession.SESSION_STATE_MAP);
+        if (res == null) {
+            res = new HashMap<String, Object>();
+            getConnectionState().put(SMTPSession.SESSION_STATE_MAP, res);
+        }
+        return res;
+    }
+
+    /**
+     * @see org.apache.james.protocols.smtp.SMTPSession#isRelayingAllowed()
+     */
+    public boolean isRelayingAllowed() {
+        return relayingAllowed;
+    }
+
+    /**
+     * @see org.apache.james.protocols.smtp.SMTPSession#resetState()
+     */
+    public void resetState() {
+        // remember the ehlo mode between resets
+        Object currentHeloMode = getState().get(CURRENT_HELO_MODE);
+
+        getState().clear();
+
+        // start again with the old helo mode
+        if (currentHeloMode != null) {
+            getState().put(CURRENT_HELO_MODE, currentHeloMode);
+        }
+    }
+
+    /**
+     * @see org.apache.james.protocols.smtp.SMTPSession#popLineHandler()
+     */
+    public void popLineHandler() {
+        if (lineHandlerCount > 0) {
+            getChannel().getPipeline().remove("lineHandler" + lineHandlerCount);
+            lineHandlerCount--;
+        }
+    }
+
+    /**
+     * @see org.apache.james.protocols.smtp.SMTPSession#pushLineHandler(org.apache.james.smtpserver.protocol.LineHandler)
+     */
+    public void pushLineHandler(LineHandler<SMTPSession> overrideCommandHandler) {
+        lineHandlerCount++;
+        // Add the linehandler in front of the coreHandler so we can be sure 
+        // it is executed with the same ExecutorHandler as the coreHandler (if one exist)
+        // 
+        // See JAMES-1277
+        getChannel().getPipeline().addBefore("coreHandler", "lineHandler" + lineHandlerCount, new LineHandlerUpstreamHandler<SMTPSession>(this, overrideCommandHandler));
+    }
+
+    /**
+     * @see org.apache.james.protocols.smtp.SMTPSession#getHelloName()
+     */
+    public String getHelloName() {
+        return theConfigData.getHelloName();
+    }
+
+    /**
+     * @see org.apache.james.protocols.smtp.SMTPSession#getMaxMessageSize()
+     */
+    public long getMaxMessageSize() {
+        return theConfigData.getMaxMessageSize();
+    }
+
+    /**
+     * @see org.apache.james.protocols.smtp.SMTPSession#getRcptCount()
+     */
+    @SuppressWarnings("unchecked")
+    public int getRcptCount() {
+        int count = 0;
+
+        // check if the key exists
+        if (getState().get(SMTPSession.RCPT_LIST) != null) {
+            count = ((Collection) getState().get(SMTPSession.RCPT_LIST)).size();
+        }
+
+        return count;
+    }
+
+    /**
+     * @see org.apache.james.protocols.smtp.SMTPSession#getSMTPGreeting()
+     */
+    public String getSMTPGreeting() {
+        return theConfigData.getSMTPGreeting();
+    }
+
+    /**
+     * @see org.apache.james.protocols.smtp.SMTPSession#isAuthSupported()
+     */
+    public boolean isAuthSupported() {
+        return theConfigData.isAuthRequired(socketAddress.getAddress().getHostAddress());
+    }
+
+    /**
+     * @see org.apache.james.protocols.smtp.SMTPSession#setRelayingAllowed(boolean)
+     */
+    public void setRelayingAllowed(boolean relayingAllowed) {
+        this.relayingAllowed = relayingAllowed;
+    }
+
+    /**
+     * @see org.apache.james.protocols.smtp.SMTPSession#sleep(long)
+     */
+    public void sleep(long ms) {
+        // session.getFilterChain().addAfter("connectionFilter",
+        // "tarpitFilter",new TarpitFilter(ms));
+    }
+
+    /**
+     * @see org.apache.james.protocols.smtp.SMTPSession#useAddressBracketsEnforcement()
+     */
+    public boolean useAddressBracketsEnforcement() {
+        return theConfigData.useAddressBracketsEnforcement();
+    }
+
+    /**
+     * @see org.apache.james.protocols.smtp.SMTPSession#useHeloEhloEnforcement()
+     */
+    public boolean useHeloEhloEnforcement() {
+        return theConfigData.useHeloEhloEnforcement();
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * org.apache.james.protocols.smtp.SMTPSession#getPushedLineHandlerCount()
+     */
+    public int getPushedLineHandlerCount() {
+        return lineHandlerCount;
+    }
+    
+}
---
+++ smtp-netty/src/main/java/org/apache/james/protocols/smtp/netty/SMTPServer.java
@@ -0,0 +1,205 @@
+/****************************************************************
+ * Licensed to the Apache Software Foundation (ASF) under one   *
+ * or more contributor license agreements.  See the NOTICE file *
+ * distributed with this work for additional information        *
+ * regarding copyright ownership.  The ASF licenses this file   *
+ * to you under the Apache License, Version 2.0 (the            *
+ * "License"); you may not use this file except in compliance   *
+ * with the License.  You may obtain a copy of the License at   *
+ *                                                              *
+ *   http://www.apache.org/licenses/LICENSE-2.0                 *
+ *                                                              *
+ * Unless required by applicable law or agreed to in writing,   *
+ * software distributed under the License is distributed on an  *
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY       *
+ * KIND, either express or implied.  See the License for the    *
+ * specific language governing permissions and limitations      *
+ * under the License.                                           *
+ ****************************************************************/
+package org.apache.james.protocols.smtp.netty;
+
+
+import javax.net.ssl.SSLContext;
+
+import org.apache.james.protocols.impl.AbstractAsyncServer;
+import org.apache.james.protocols.impl.AbstractSSLAwareChannelPipelineFactory;
+import org.apache.james.protocols.smtp.SMTPConfiguration;
+import org.apache.james.protocols.smtp.SMTPProtocolHandlerChain;
+import org.apache.james.protocols.smtp.SMTPServerMBean;
+import org.jboss.netty.channel.ChannelPipelineFactory;
+import org.jboss.netty.channel.ChannelUpstreamHandler;
+import org.jboss.netty.channel.group.ChannelGroup;
+import org.jboss.netty.handler.codec.oneone.OneToOneEncoder;
+import org.jboss.netty.handler.execution.ExecutionHandler;
+import org.jboss.netty.handler.execution.OrderedMemoryAwareThreadPoolExecutor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * NIO SMTPServer which use Netty
+ */
+public class SMTPServer extends AbstractAsyncServer implements SMTPServerMBean {
+
+    private SMTPProtocolHandlerChain chain;
+    
+    private Logger logger = LoggerFactory.getLogger(SMTPServer.class);
+
+    private SSLContext context;
+
+    private boolean starttls;
+    
+    private ExecutionHandler eHandler;
+
+   
+    /**
+     * The configuration data to be passed to the handler
+     */
+    private final SMTPConfiguration theConfigData;
+
+
+    private final static SMTPResponseEncoder SMTP_RESPONSE_ENCODER = new SMTPResponseEncoder();
+    private SMTPChannelUpstreamHandler coreHandler;
+
+   
+    
+    public SMTPServer(SMTPConfiguration theConfigData, SMTPProtocolHandlerChain chain) {
+        this(theConfigData, chain, null, false);
+    }
+    
+    
+    public SMTPServer(SMTPConfiguration theConfigData, SMTPProtocolHandlerChain chain, SSLContext context, boolean starttls) {
+        super();
+        this.chain  = chain;
+        this.context = context;
+        this.starttls = starttls;
+        if (context != null && starttls) {
+            this.theConfigData = new StartTLSSMTPConfiguration(theConfigData);
+        } else {
+            this.theConfigData = theConfigData;
+        }
+    }
+    
+    protected ExecutionHandler createExecutionHandler(int size) {
+        return new ExecutionHandler(new OrderedMemoryAwareThreadPoolExecutor(size, 0, 0));
+    }
+    
+    
+    public void setUseExecutionHandler(boolean useHandler, int size) {
+        if (isBound()) throw new IllegalStateException("Server running already");
+        if (useHandler) {
+            eHandler =createExecutionHandler(size);
+        } else {
+            if (eHandler != null) {
+                eHandler.releaseExternalResources();
+            }
+            eHandler = null;
+        }
+    }
+    
+  
+    protected ChannelUpstreamHandler createCoreHandler() {
+        return coreHandler;
+    }
+
+
+    /*
+     * (non-Javadoc)
+     * @see org.apache.james.protocols.smtp.SMTPServerMBean#isEnabled()
+     */
+    public boolean isEnabled() {
+        return isBound();
+    }
+
+    /*
+     * (non-Javadoc)
+     * @see org.apache.james.protocols.smtp.SMTPServerMBean#getSocketType()
+     */
+    public String getSocketType() {
+        if (context != null && !starttls) {
+            return "ssl";
+        } else {
+            return "plain";
+        }
+    }
+
+    
+    @Override
+    public synchronized void bind() throws Exception {
+        coreHandler = new SMTPChannelUpstreamHandler(chain, theConfigData, logger, context, null);
+        super.bind();
+    }
+
+
+    @Override
+    protected ChannelPipelineFactory createPipelineFactory(ChannelGroup group) {
+        return new AbstractSSLAwareChannelPipelineFactory(getTimeout(), 0, getBacklog(), group, eHandler) {
+
+            @Override
+            protected ChannelUpstreamHandler createHandler() {
+                return coreHandler;
+            }
+
+            @Override
+            protected OneToOneEncoder createEncoder() {
+                return SMTP_RESPONSE_ENCODER;
+            }
+
+            @Override
+            protected boolean isSSLSocket() {
+                return context != null && !starttls;
+            }
+
+            @Override
+            protected SSLContext getSSLContext() {
+                return context;
+            }
+        };
+
+    }
+
+    private final static class StartTLSSMTPConfiguration implements SMTPConfiguration {
+
+        private SMTPConfiguration config;
+
+        public StartTLSSMTPConfiguration(SMTPConfiguration config) {
+            this.config = config;
+        }
+        
+        public String getHelloName() {
+            return config.getHelloName();
+        }
+
+        public int getResetLength() {
+            return config.getResetLength();
+        }
+
+        public long getMaxMessageSize() {
+            return config.getMaxMessageSize();
+        }
+
+        public boolean isRelayingAllowed(String remoteIP) {
+            return config.isRelayingAllowed(remoteIP);
+        }
+
+        public boolean isAuthRequired(String remoteIP) {
+            return config.isAuthRequired(remoteIP);
+        }
+
+        public boolean useHeloEhloEnforcement() {
+            return config.useHeloEhloEnforcement();
+        }
+
+        public String getSMTPGreeting() {
+            return config.getSMTPGreeting();
+        }
+
+        public boolean useAddressBracketsEnforcement() {
+            return config.useAddressBracketsEnforcement();
+        }
+
+        public boolean isStartTLSSupported() {
+            return true;
+        }
+        
+    }
+}

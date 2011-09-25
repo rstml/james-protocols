96a1335f287087fe8c9c5aef186a1ae52e0a8c92
Norman Maurer <norman@apache.org>
2011-09-25 18:00:33 +0000

    Committed by mistake
    git-svn-id: https://svn.apache.org/repos/asf/james/protocols/trunk@1175447 13f79535-47bb-0310-9956-ffa450edef68

--- impl/src/main/java/org/apache/james/protocols/impl/NettyConstants.java
+++
@@ -1,7 +0,0 @@
-package org.apache.james.protocols.impl;
-
-public interface NettyConstants {
-
-	public final static String TRANSPORT = "TRANSPORT";
-	public final static String SESSION = "SESSION";
-}

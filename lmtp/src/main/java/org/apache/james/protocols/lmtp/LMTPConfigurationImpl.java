66ae50ba44b316dcc06aea50542bd70acc6cb5a7
Norman Maurer <norman@apache.org>
2011-10-27 16:29:10 +0000

    Simplify starttls configuration
    git-svn-id: https://svn.apache.org/repos/asf/james/protocols/trunk@1189846 13f79535-47bb-0310-9956-ffa450edef68

--- lmtp/src/main/java/org/apache/james/protocols/lmtp/LMTPConfigurationImpl.java
+++ lmtp/src/main/java/org/apache/james/protocols/lmtp/LMTPConfigurationImpl.java
@@ -23,6 +23,7 @@ public class LMTPConfigurationImpl extends LMTPConfiguration{
     private String helloName = "localhost";
     private long maxMessageSize = 0;
     private String greeting = "JAMES Protocols LMTP Server";
+	private boolean startTLSsupported = false;
 
     @Override
     public String getHelloName() {
@@ -51,5 +52,14 @@ public class LMTPConfigurationImpl extends LMTPConfiguration{
     
     public void setGreeting(String greeting) {
         this.greeting = greeting;
+    }
+    
+    @Override
+    public boolean isStartTLSSupported() {
+        return startTLSsupported;
+    }
+
+    public void setStartTLSSupported(boolean startTLSsupported) {
+        this.startTLSsupported  = startTLSsupported;
     }
 }
